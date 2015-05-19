61eeb1174163633a33f4f16e00f646504dc6a703
linnbatelson <linn@batelson.se>
2015-05-19 12:50:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> CourseTime </title>
+    <link rel="stylesheet" href="assets/main.css">
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div id="content">
+      <div id="header">
+        <div id="logga">CourseTime logga</div>
+        <div id="login-reg"> Logga in/registrera </div>
+      </div>
+      <div id="upper">
+        <form action="#" method="GET" class="searchfield">
+          <label for="inputsearch"><h1> Sök kurs </h1></label>
+          <input type="text" placeholder="  Skriv in sökning här..." id="inputsearch" name="search">
+          <input type="submit" value="Sök" id="searchBtn" class="searchBtn">
+        </form>
+      </div>
+      <div id="main">
+        <div id= "middle">Mittenfältet</div>
+        <div id="rightnav" class="sidebar"> Höger sidebar</div>
+        <div id="leftnav" class="sidebar">Vänster sidebar</div>
+      </div>
+    </div>
+    <div id="footer">© CourseTime 2015 Grupp 16</div>
+  </body>
+</html>
