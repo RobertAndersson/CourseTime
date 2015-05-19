7e9ee9221cd2e4be746220163506b203af89475e
linnbatelson <linn@batelson.se>
2015-05-19 22:28:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,21 @@
         <div id="login-reg"> Logga in/registrera </div>
       </div>
       <div id="upper">
-        <form action="#" method="POST" class="searchfield">
+        <form action="index.php" method="POST" class="searchfield">
           <label for="inputsearch"><h1> Sök kurs </h1></label>
           <input type="text" placeholder="  Skriv din sökning här..." id="inputsearch" name="search">
           <input type="submit" value="Sök" id="searchBtn" class="searchBtn">
         </form>
       </div>
       <div id="main">
-        <div id= "middle">Mittenfältet</div>
+        <div id= "middle">Mittenfältet
+          <div id="courses">
+            <p id="readComments" class="field">
+              <h2>Kurser</h2>
+            </p>
+            <?php include "search.php" ?>
+          </div>
+        </div>
         <div id="rightnav" class="sidebar"> Höger sidebar</div>
         <div id="leftnav" class="sidebar">Vänster sidebar</div>
       </div>
