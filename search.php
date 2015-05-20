6297ad0a7f51f39df5feef90ed9b5949cf9d62ad
linnbatelson <linn@batelson.se>
2015-05-20 11:35:20 +0200

    Update search.php
    Uppdaterad med sökfunktion för högskolepoäng. Inte färdigt!

--- search.php
+++ search.php
@@ -1,30 +1,56 @@
 <?php
 
+$output = "";
+
   if (isset($_POST['submit'])) 
   {
 	if (preg_match(" /[A-Za-z]+/", $_POST['search'])) 
 	{
 		$search=$_POST['search'];
-		$host = 'localhost:8889';
-		$user = 'root';
-		$password = 'root';
-		$db = 'projekt';
- 
-		$dbconn = new mysqli($host, $user, $password, $db) or die("Kunde inte connecta till databasen");
-		$sql = "SELECT DISTINCT * FROM courses WHERE courseName LIKE '%" . $search . "%' OR courseSubject LIKE '%" . $search . "%'";
-		$result= $dbconn->query($sql);
-		
-		$output ="";
-		while ($row = $result->fetch_assoc()) 
-		{ ?>
-			<div class="showcourse" id="showcourse"> <span id="course">Kurs:</span> <?php echo $row['courseName'];?> </div>
+
+
+		if ($_POST['hpfilter'] == "5hp") {
 			
-<?php   }
+			$sql = "(SELECT * FROM courses WHERE courseName LIKE '%" . $search . "%' OR courseSubject LIKE '%" . $search ."%' AND courseLength = '5hp')";
+		}
+
+		elseif ($_POST['hpfilter'] == "7.5hp") {
+			
+			$sql = "(SELECT * FROM courses WHERE courseName LIKE '%" . $search . "%' OR courseSubject LIKE '%" . $search ."%' AND courseLength = '7,5hp')";
+		}
+
+		elseif ($_POST['hpfilter'] == "15hp") {
+			
+			$sql = "(SELECT * FROM courses WHERE courseName LIKE '%" . $search . "%' OR courseSubject LIKE '%" . $search ."%' AND courseLength = '15hp')";
+		}
+
+		elseif ($_POST['hpfilter'] == "30hp") {
+			
+			$sql = "(SELECT * FROM courses WHERE courseName LIKE '%" . $search . "%' OR courseSubject LIKE '%" . $search ."%' AND courseLength = '30hp')";
+		}
+
+		else {
+			$sql = "SELECT * FROM courses WHERE courseName LIKE '%" . $search . "%' OR courseSubject LIKE '%" . $search ."%'";
+		}
+		
+		$result= $db->connection->query($sql);
+
+		if ($result->num_rows > 0) 
+		{
+			while ($row = $result->fetch_assoc()) 
+			{ 
+				$course = $row["courseName"];
+		    		$pace = $row["coursePace"];
+		    		$output = "$course - $pace <br />";
+			}
+			echo $output;
+		}
+		
+		else {
+			echo "Inga matchningar på din sökning.";
+		}
+		
 	}
   }
- 
-  else
-  {
-  	echo "<p> Skriv något i sökfältet</p>";
-  }
+
 ?>
