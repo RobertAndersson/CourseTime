c78216498df772b80f49cd972da6fa16b8c00777
linnbatelson <linn@batelson.se>
2015-05-20 11:37:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,26 @@
           <label for="inputsearch"><h1> Sök kurs </h1></label>
           <input type="text" placeholder="  Skriv din sökning här..." id="inputsearch" name="search">
           <input type="submit" value="Sök" id="searchBtn" class="searchBtn">
+          <div id="filtersection"> Filtrera din sökning
+            <select name="hpfilter">
+              <option value="högskolepoäng">Högskolepoäng</option>
+              <option value="5hp">5</option>
+              <option value="7.5hp">7,5</option>
+              <option value="15hp">15</option>
+              <option value="30hp">30</option>
+            </select>
+            <select name="pacefilter">
+              <option value="level">Nivå</option>
+              <option value="grund">Grundnivå</option>
+              <option value="avancerad">Avancerad nivå</option>
+            </select>
+            <select name="levelfilter">
+              <option value="pace">Studietakt</option>
+              <option value=">=25">Upp till och med 25%</option>
+              <option value=">=50">Upp till och med 50%</option>
+              <option value="100">Heltid (100%)</option>
+            </select>
+          </div>
         </form>
       </div>
       <div id="main">
